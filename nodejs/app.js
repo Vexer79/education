ba76d915116cd7fa783eb32dc12f14256f49f082
Vexer79 <rokytskyi.i@gmail.com>
2024-03-27 07:02:08 +0200

    feat: add responce to nodejs server

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,7 +1,13 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req, res) =>{
-    console.log(req);
+const server = http.createServer((req, res) => {
+    res.write(`
+    <html>
+    <head><title>First app</title></head>
+    <body><h1>url - ${req.url}, method - ${req.method}, headers - ${JSON.stringify(req.headers)}</h1></body>
+    </html>
+    `);
+    res.end();
 });
 
 server.listen(3000);
