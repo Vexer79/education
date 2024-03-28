863833f70a869b47c0c6a60a571c81d8cb2e1046
Vexer79 <rokytskyi.i@gmail.com>
2024-03-28 22:02:07 +0200

    feat: add request listeners

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,27 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+    if(req.method === 'POST'){
+        const body = [];
+        req.on("data", (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        })
+        req.on("end", ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync("message.txt", message);
+        });
+        return res.end();
+    }
     res.write(`
     <html>
     <head><title>First app</title></head>
-    <body><h1>url - ${req.url}, method - ${req.method}, headers - ${JSON.stringify(req.headers)}</h1></body>
+    <body>
+    <h1>url - ${req.url}, method - ${req.method}, headers - ${JSON.stringify(req.headers)}</h1>
+    <form action="/" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>
+    </body>
     </html>
     `);
     res.end();
